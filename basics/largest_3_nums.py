95062f262a904556a28f1b28f7cfa54f384265e8
srikanthpragada <srikanthpragada@gmail.com>
2026-03-13 20:54:05 +0530

    13-MAR

---
+++ basics/largest_3_nums.py
@@ -0,0 +1,11 @@
+# Largest of 3 numbers
+
+a, b, c = 10, 10, 10
+
+if a > b and a > c:
+    print(a)
+elif b > c:
+    print(b)
+else:
+    print(c)
+
---
+++ basics/while_demo.py
@@ -0,0 +1,10 @@
+# Print numbers from 1 to 10
+
+n = 1
+while n <= 10:
+    print(n, end = ' ')
+    n = n + 1
+
+print('The End!')
+
+
